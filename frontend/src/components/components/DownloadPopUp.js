2e1cbc8a83a103356ebc4652f1b795a05993e2d0
GithAndrew <79851346+GithAndrew@users.noreply.github.com>
2024-04-03 17:28:48 +0800

    fix: upload image for csv files

--- frontend/src/components/components/DownloadPopUp.js
+++ frontend/src/components/components/DownloadPopUp.js
@@ -10,10 +10,11 @@ import '../css/PopUp.css'
 function DownloadPopUp (props) {
 
     const [csvFile, setFile] = useState();
-    const [appData, setAppData] = useState({});
     const allEmails = props.allEmails;
     const [showAlert, setShowAlert] = useState(false);
     const [alertMessage, setAlertMessage] = useState('');
+    const [picID, setPicID] = useState(null);
+    const [csvImage, setcsvImage] = useState(null);
 
     const showMessage = (message) => {
         setAlertMessage(message);
@@ -136,19 +137,50 @@ function DownloadPopUp (props) {
         }
 
         if (boolData === true){
-            setAppData(data)
-            if(props.user === "donor") {sendDonorData()}
-            if(props.user === "scholar") {sendScholarData(siblings, education)}
+            if(props.user === "donor") {sendDonorData(data)}
+            if(props.user === "scholar") {sendScholarData(data, siblings, education)}
         }
     }
 
-    const sendScholarData = (siblings, education) => {
+    const openImageFilePopup = (e) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const img = new Image();
+            img.onload = () => {
+                if (img.width !== img.height) {
+                    showMessage('Please upload a square image.');
+                    setcsvImage(null);
+                } else {
+                    setcsvImage(reader.result);
+                    const data = new FormData();
+                    data.append("image", e.target.files[0]);
+
+                    fetch(apiUrl("/upload"), {
+                      method: "POST",
+                      body: data,
+                      credentials: 'include'
+                    }).then((response) => response.json())
+                    .then((result) => {
+                        setPicID(result.id);
+                    });
+                }
+            };
+            img.src = reader.result;
+        };
+        reader.readAsDataURL(e.target.files[0]);
+    }
+
+    console.log(csvImage)
+
+    const sendScholarData = (appData, siblings, education) => {
+        if (picID === null) {
+            showMessage('No image set!');
+            return
+        }
+
         const tempEmail = appData["email"];
         const uniqueEmail = !allEmails.includes(tempEmail);
 
-        console.log(siblings)
-        console.log(education)
-    
         if (uniqueEmail) {
             const address = {
                 street: appData["street_name & house no."],
@@ -159,9 +191,9 @@ function DownloadPopUp (props) {
                 postal_code: appData["postal_code"]
             };
 
-            const father_details = getPersonDetails("father's");
-            const mother_details = getPersonDetails("mother's");
-            const guardian_details = getPersonDetails("guardian's");
+            const father_details = getPersonDetails(appData, "father's");
+            const mother_details = getPersonDetails(appData, "mother's");
+            const guardian_details = getPersonDetails(appData, "guardian's");
 
             fetch(apiUrl("/applicant"), {
                 method: "POST",
@@ -189,7 +221,7 @@ function DownloadPopUp (props) {
                     sibling_details: siblings,
                     educational_bg: education,
                     statement: appData["statement"],
-                    upload_id: ""
+                    upload_id: picID
                 })
             })
             .then(response => response.json())
@@ -202,7 +234,7 @@ function DownloadPopUp (props) {
         }
     }
 
-    const getPersonDetails = (parent) => ({
+    const getPersonDetails = (appData, parent) => ({
         [`${parent.replace("'s","")}_name`]: appData[`${parent}_name`],
         [`${parent.replace("'s","")}_birthday`]: appData[`${parent}_birthdate`],
         [`${parent.replace("'s","")}_citizenship`]: appData[`${parent}_citizenship`],
@@ -214,7 +246,12 @@ function DownloadPopUp (props) {
         [`${parent.replace("'s","")}_income`]: appData[`${parent}_annual income`]
     });
 
-    const sendDonorData = () => {
+    const sendDonorData = (appData) => {
+        if (picID === null) {
+            showMessage('No image set!');
+            return
+        }
+
         const tempEmail = appData["email"];
         const uniqueEmail = !allEmails.includes(tempEmail);
 
@@ -237,36 +274,21 @@ function DownloadPopUp (props) {
                     sex: appData["sex"],
                     citizenship: appData["citizenship"],
                     statement: appData["statement"],
-                    upload_id: ""
+                    upload_id: picID
                 })
             })
             .then(response => response.json())
-            .then(getDonorData)
+            .then(data => {sendScholarship(appData, data.donor._id);})
             .then(showMessage(`Application for ${appData["first_name"]} ${appData["last_name"]} accepted!`))
-            .then()
             .catch(error => {
                 console.error('Error submitting application:', error);
             });
-            setTimeout(() => window.location.reload(), 750)
+            // setTimeout(() => window.location.reload(), 750)
         } else {showMessage("Inputted email address already exists!")}
     }
 
-    const getDonorData = () => {
-        Promise.all([
-            fetch(apiUrl(`/donor`))
-        ])
-        .then(([resDonors]) => {
-            return Promise.all([resDonors.json()]);
-        })
-        .then(([dataDonors]) => {
-            sendScholarship(dataDonors[dataDonors.length - 1]);
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        });
-    }
-
-    const sendScholarship = (donorData) => {
+    const sendScholarship = (appData, donorID) => {
+        console.log(appData)
         fetch(apiUrl("/scholarship"), {
             method: "POST",
             credentials: 'include',
@@ -279,7 +301,7 @@ function DownloadPopUp (props) {
                 scholarshipname: appData["scholarship_name"],
                 details: "(" + appData["details"]+ ")",
                 year: appData["years_involved"],
-                donor_id: donorData._id
+                donor_id: donorID
             })
         })
         .then(response => { return response.json() })
@@ -293,18 +315,31 @@ function DownloadPopUp (props) {
                 <div className="upload-popup-box">
                     <span className="add-close-icon" onClick={props.handleClose}>x</span>
                     <p className='delete-label'>Instructions for Application</p>
-                    <p className='form-subtitle'>1. Download the Excel file through the blue button found below.</p>
-                    <p className='form-subtitle'>2. Fill up your data. Cells with a * means information is required.</p>
-                    <p className='form-subtitle'>3. Save the file as .csv.</p>
-                    <p className='form-subtitle'>4. Upload your CSV file using the green button.</p>
-                    <p className='form-subtitle'>5. Another button will appear for you to submit your file.</p>
+                    <p className='popup-form-subtitle'>1. Download the Excel file through the blue button found below.</p>
+                    <p className='popup-form-subtitle'>2. Fill up your data. Cells with a * means information is required.</p>
+                    <p className='popup-form-subtitle'>3. Save the file as .csv.</p>
+                    <p className='popup-form-subtitle'>4. Upload your CSV file using the green button.</p>
+                    <p className='popup-form-subtitle'>5. If the proper data is sent, a button for uploading your image will appear.</p>
+                    <p className='popup-form-subtitle'>6. Another button will appear for you to submit your file.</p>
+
+                    {csvImage && (
+                        <div className='popup-uploaded-image'>
+                            <img src={csvImage} alt="Upload" />
+                        </div>
+                    )}
 
                     <div className="delete-buttons">
-                        {props.user === "donor" ? <a href={donortemplate} className='upload-blue-button' download> Download Excel</a>: ""}
-                        {props.user === "scholar" ? <a href={scholartemplate} className='upload-blue-button' download> Download Excel</a>: ""}
-                        <label htmlFor="file-input" className='upload-green-button'>Upload CSV</label>
+                        {props.user === "donor" ? <a href={donortemplate} className='upload-green-button' download> Download Excel</a>: ""}
+                        {props.user === "scholar" ? <a href={scholartemplate} className='upload-green-button' download> Download Excel</a>: ""}
+                        <label htmlFor="file-input" className='upload-blue-button'>Upload CSV</label>
                         <input type="file" id="file-input" accept='.csv' onChange={setCSVFile}></input>
-                        {csvFile ? <button className='upload-green-button' onClick={() => readFile(csvFile)}>Submit {csvFile.name}</button> : "" }
+                        {csvFile ? 
+                            <div className='upload-photo-box'>
+                                <label htmlFor="upload-photo-popup" className="upload-label">Upload Picture</label>
+                                <input type="file" id="upload-photo-popup" accept=".png,.jpg" onChange={openImageFilePopup}/>
+                            </div>
+                        : ""}
+                        {csvImage ? <button className='upload-blue-button' onClick={() => readFile(csvFile)}>Submit {csvFile.name}</button> : "" }
                     </div>
 
                 </div>
