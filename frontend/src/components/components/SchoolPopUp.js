16f15dba0f530c23f1e07384ad644fe29931aacb
GithAndrew <79851346+GithAndrew@users.noreply.github.com>
2024-04-22 13:24:02 +0800

    debug: print result.id

--- frontend/src/components/components/SchoolPopUp.js
+++ frontend/src/components/components/SchoolPopUp.js
@@ -151,6 +151,7 @@ function SchoolPopUp () {
                       credentials:'include'
                     }).then((response) => response.json())
                     .then((result) => {
+                        console.log(result.id);
                         setPicID(result.id);
                     });
                 }
