2908f3f8d7d50ab613cc12826d98e24ee93264ea
GithAndrew <79851346+GithAndrew@users.noreply.github.com>
2024-04-24 18:51:24 +0800

    fix: final debugging for deployment login

--- backend/controllers/user_ctrl.js
+++ backend/controllers/user_ctrl.js
@@ -60,10 +60,10 @@ exports.login = async (req, res) => {
 };
 
 exports.isLogin = async (req, res) => {
-    // if (!req.cookies || !req.cookies.authToken) {
-    //     console.log('Unauthorized access');
-    //     return res.status(200).send({ message: 'Unauthorized access', status: false });
-    // }
+    if (!req.cookies || !req.cookies.authToken) {
+        console.log('Unauthorized access');
+        return res.status(200).send({ message: 'Unauthorized access', status: false });
+    }
 
     let tokenDetails = await utils.verifyToken(req);
 
