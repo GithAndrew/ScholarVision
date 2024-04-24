b68a71b4fed9ba7c0b2b67bbcf576d04a48d06c6
GithAndrew <79851346+GithAndrew@users.noreply.github.com>
2024-04-24 16:05:26 +0800

    debug: remove logo

--- frontend/src/components/components/Header.js
+++ frontend/src/components/components/Header.js
@@ -60,7 +60,8 @@ const Header = () => {
     return(
         <header className='main-header'>
             <img className="main-logo" src={SVLogo} alt="logo"/>
-            {school.upload_id ? <img className="main-logo" src={require(`../images/${school.upload_id}`)} alt="logo"/> : <img className="main-logo" src={SchoolLogo} alt="logo"/>}
+            {/* {school.upload_id ? <img className="main-logo" src={require(`../images/${school.upload_id}`)} alt="logo"/> : <img className="main-logo" src={SchoolLogo} alt="logo"/>} */}
+            <img className="login-logo" src={SchoolLogo} alt="logo"/>
             <div>
                 {school.school_name ? <p className='header-text'style={{fontSize: '1.5em'}}><Link to="/Home">{school.school_name}</Link></p> : 
                     <p className='header-text'style={{fontSize: '1.5em'}}><Link to="/Home">School Name</Link></p>
--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -109,7 +109,8 @@ function Login() {
     <div className="login-limiter">
       <header className='login-header'>
         <img className="login-logo" src={SVLogo} alt="logo" />
-        {school.upload_id ? <img className="login-logo" src={require(`../images/${school.upload_id}`)} alt="logo"/> : <img className="login-logo" src={SchoolLogo} alt="logo"/>}
+        {/* {school.upload_id ? <img className="login-logo" src={require(`../images/${school.upload_id}`)} alt="logo"/> : <img className="login-logo" src={SchoolLogo} alt="logo"/>} */}
+        <img className="login-logo" src={SchoolLogo} alt="logo"/>
         <div>
           {school.school_name ? <p className='login-header-text'>{school.school_name}</p> : 
             <p className='login-header-text'style={{fontSize: '1.5em'}}>Name of School</p>
